ac212d9eee93d268d39152d57319d5b2b7bda642
mudy1993 <mudy@liuna.tianxiang.txsec.com>
2015-09-24 18:00:16 +0800

    tableview加载出来了，但是透视图咩有

--- Shopping/首页/controller/ShouYeTableViewController.swift
+++ Shopping/首页/controller/ShouYeTableViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class ShouYeTableViewController: UITableViewController{
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        title = "首页"
+        //        title = "首页"
         navigationItem.leftBarButtonItem = setCitySelectedBtn()
         setNavTitleView()
-      
+        
     }
-
+    
     func setCitySelectedBtn()->UIBarButtonItem{
         //设置主页左上方选择城市的按钮
         let cityBtn = UIButton.buttonWithType(UIButtonType.Custom) as! UIButton
@@ -34,23 +34,23 @@ class ShouYeTableViewController: UITableViewController{
         
         cityBtn.frame = CGRectMake(0, 5, 54, 30)
         cityBtn.addTarget(self, action: "citySelectedBtn", forControlEvents: UIControlEvents.TouchUpInside)
-       //当设置了左边的按钮自定义以后，titleview就不居中了
+        //当设置了左边的按钮自定义以后，titleview就不居中了
         let cityBtnItem = UIBarButtonItem(customView: cityBtn)
         
         return cityBtnItem
     }
     
     func citySelectedBtn() {
-   print("你点击了城市选择的按钮\n")
+        print("你点击了城市选择的按钮\n")
         
         //设置主页左上方选择城市的按钮
         
         
     }
-
-  
-   
-
+    
+    
+    
+    
     
     func setNavTitleView(){
         //设置首页导航栏的中间的视图
@@ -66,82 +66,93 @@ class ShouYeTableViewController: UITableViewController{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
     
-     override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
         let header = NSBundle.mainBundle().loadNibNamed("headView", owner: self, options: nil).last as! UIView
         
-    return header
+        return header
     }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
+    
+    
+    // MARK: - Table view data source
+    
+    
+    //    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    //        // #warning Potentially incomplete method implementation.
+    //        // Return the number of sections.
+    //        return 0
+    //    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return 5
     }
-
     
     
-    /*
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+//        let identify = "indetify" as String
+//        
+//        let cell = tableView.dequeueReusableCellWithIdentifier(identify, forIndexPath: indexPath) as! UITableViewCell
+//        
 
+        let cell = UITableViewCell(style:.Default, reuseIdentifier:"myCell")
+        
+        cell.textLabel!.text = "swift cell \(indexPath.row)"
+        
+        return cell
         // Configure the cell...
-
+        
         return cell
     }
-    */
-
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    // Return NO if you do not want the specified item to be editable.
+    return true
     }
     */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    // Return NO if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
